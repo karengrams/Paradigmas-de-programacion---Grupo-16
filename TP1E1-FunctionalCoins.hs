bf896be3af46e7048f9f99e33e07b800ec23500f
Unknown <kmanrique@est.frba.utn.edu.ar>
2018-04-08 23:23:04 -0300

    Modificaciones + clientes prueba
    Le hice un par de modificaciones a "extraer" y "upgrade" y de paso le agregue los clientes "prueba" (con uno demás just in case). Hice un par de comentarios para no olvidarme y tambien puse como comentario el codigo anterior.

--- TP1E1-FunctionalCoins.hs
+++ TP1E1-FunctionalCoins.hs
@@ -6,25 +6,51 @@
 import Text.Show.Functions -- Para mostrar <Function> en consola cada vez que devuelven una.
 import Data.List -- Para métodos de colecciones que no vienen por defecto.
 import Data.Maybe -- Por si llegan a usar un método de colección que devuelva “Just suElemento” o “Nothing”.
-import Test.Hspec -- Para poder usar los tests que se piden más abajo.
+{-import Test.Hspec -- Para poder usar los tests que se piden más abajo.-}
 
-data Billetera = Billetera {nombre :: String, dinero :: Float} deriving (Show, Eq)
+data Billetera = Billetera {
+nombre :: String, 
+dinero :: Float
+} deriving (Show, Eq)
 
 type Evento = Billetera -> Billetera
+
+sinDinero billetera = billetera { dinero = 0 }
 
 depositar :: Float -> Evento
 depositar dineroDepositado billetera = billetera {dinero = (dinero billetera + dineroDepositado)} 
 
 extraer :: Float -> Evento
+extraer dineroExtraido billetera 
+  | dinero billetera + dineroExtraido <=0 = sinDinero billetera
+  | otherwise = billetera { dinero = (dinero billetera + dineroExtraido)}
+
+--Aca lo unico que cambie es lo del 0, lo de "billetera {dinero = (max) 0 (dinero billetera + dineroExtraido)", no lo intente, pero me gustaria ver si funciona, queda muchisimo mejor y más corto, que la forma que yo puse
+
+{-
+extraer :: Float -> Evento
 extraer dineroExtraido billetera = billetera {dinero = (max) 0 (dinero billetera + dineroExtraido)}
--- Por el enunciado entiendo que en una extraccion el monto se ingresa negativo, por eso lo sumo al monto original.
+-- Por el enunciado entiendo que en una extraccion el monto se ingresa negativo, por eso lo sumo al monto original. 
+-}
+
 
 upgrade :: Evento
-upgrade billetera = billetera {dinero = (dinero billetera + (min) 10 (0.20 * dinero billetera))}
+upgrade billetera 
+  | (dinero billetera) * 0.2 <= 10 = billetera { dinero = (dinero billetera * 1.2) }
+  | otherwise = billetera { dinero = (dinero billetera + 10)}
+  
+--Estuve viendo en el grupo que hay en la pagina del curso y preguntaron por esto, supuestamente se la agrega 10 si se supera, sino, se le agrega el 20%. Sigo con mis dudas igual, el martes le preguntamos a Santi.
 
+  {-
+  upgrade billetera = billetera {dinero = (dinero billetera + (min) 10 (0.20 * dinero billetera))}
+  -}
+ 
 cerrarCuenta :: Evento
-cerrarCuenta billetera = billetera {dinero = 0}
+cerrarCuenta billetera = sinDinero billetera
 
 quedaIgual ::Evento
 quedaIgual billetera = billetera
 
+unaBilletera = Billetera "Facundo" 1023
+billeteraPepe = Billetera "Jose" 10
+billeteraLucho = Billetera "Luciano" 2
