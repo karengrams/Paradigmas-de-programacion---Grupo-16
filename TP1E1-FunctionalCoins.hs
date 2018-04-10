bf2d3e3f0390c5af887ffbafdf71046eabe81cdb
matiastomasone <matias.tomasone@gmail.com>
2018-04-10 13:55:47 -0300

    Terminaciones - Puntos Faltantes

--- TP1E1-FunctionalCoins.hs
+++ TP1E1-FunctionalCoins.hs
@@ -4,12 +4,12 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 import Text.Show.Functions -- Para mostrar <Function> en consola cada vez que devuelven una.
-import Data.List -- Para métodos de colecciones que no vienen por defecto.
-import Data.Maybe -- Por si llegan a usar un método de colección que devuelva “Just suElemento” o “Nothing”.
-{-import Test.Hspec -- Para poder usar los tests que se piden más abajo.-}
+import Data.List -- Para mï¿½todos de colecciones que no vienen por defecto.
+import Data.Maybe -- Por si llegan a usar un mï¿½todo de colecciï¿½n que devuelva ï¿½Just suElementoï¿½ o ï¿½Nothingï¿½.
+{-import Test.Hspec -- Para poder usar los tests que se piden mï¿½s abajo.-}
 
 data Billetera = Billetera {
-nombre :: String, 
+nombre :: String,
 dinero :: Float
 } deriving (Show, Eq)
 
@@ -18,33 +18,17 @@ type Evento = Billetera -> Billetera
 sinDinero billetera = billetera { dinero = 0 }
 
 depositar :: Float -> Evento
-depositar dineroDepositado billetera = billetera {dinero = (dinero billetera + dineroDepositado)} 
+depositar dineroDepositado billetera = billetera {dinero = (dinero billetera + dineroDepositado)}
 
 extraer :: Float -> Evento
-extraer dineroExtraido billetera 
+extraer dineroExtraido billetera
   | dinero billetera + dineroExtraido <=0 = sinDinero billetera
   | otherwise = billetera { dinero = (dinero billetera + dineroExtraido)}
 
---Aca lo unico que cambie es lo del 0, lo de "billetera {dinero = (max) 0 (dinero billetera + dineroExtraido)", no lo intente, pero me gustaria ver si funciona, queda muchisimo mejor y más corto, que la forma que yo puse
 
-{-
-extraer :: Float -> Evento
-extraer dineroExtraido billetera = billetera {dinero = (max) 0 (dinero billetera + dineroExtraido)}
--- Por el enunciado entiendo que en una extraccion el monto se ingresa negativo, por eso lo sumo al monto original. 
--}
+upgrade billetera = billetera {dinero = (dinero billetera + (min) 10 (0.20 * dinero billetera))}
 
 
-upgrade :: Evento
-upgrade billetera 
-  | (dinero billetera) * 0.2 <= 10 = billetera { dinero = (dinero billetera * 1.2) }
-  | otherwise = billetera { dinero = (dinero billetera + 10)}
-  
---Estuve viendo en el grupo que hay en la pagina del curso y preguntaron por esto, supuestamente se la agrega 10 si se supera, sino, se le agrega el 20%. Sigo con mis dudas igual, el martes le preguntamos a Santi.
-
-  {-
-  upgrade billetera = billetera {dinero = (dinero billetera + (min) 10 (0.20 * dinero billetera))}
-  -}
- 
 cerrarCuenta :: Evento
 cerrarCuenta billetera = sinDinero billetera
 
@@ -53,4 +37,40 @@ quedaIgual billetera = billetera
 
 unaBilletera = Billetera "Facundo" 1023
 billeteraPepe = Billetera "Jose" 10
-billeteraLucho = Billetera "Luciano" 2
+billeteraPepe2 = Billetera "Jose" 20
+billeteraPepe3 = Billetera "Jose" 50
+billeteraLucho = Billetera "Luciano" 10
+
+transaccionUno :: Evento
+transaccionUno billetera | nombre billetera == "Luciano" =  cerrarCuenta billetera
+                         | otherwise = quedaIgual billetera
+
+transaccionDos :: Evento
+transaccionDos billetera | nombre billetera == "Jose" =  depositar 5 billetera
+                         | otherwise = quedaIgual billetera
+
+
+type Transaccion = String -> Evento -> Evento
+
+transaccion :: Transaccion
+transaccion nombreAComparar eventoAAplicar billeteraAfectada | nombre billeteraAfectada == nombreAComparar = eventoAAplicar billeteraAfectada
+                                                                    | otherwise = quedaIgual billeteraAfectada
+
+tocoYMeVoy :: Evento
+tocoYMeVoy billetera = (cerrarCuenta . upgrade . depositar 15) billetera
+
+ahorranteErrante :: Evento
+ahorranteErrante billetera = (depositar 10 . upgrade . depositar 8 . extraer (-1) . depositar 2 . depositar 1) billetera
+
+
+transaccionTres :: Evento
+transaccionTres billetera | nombre billetera == "Luciano" =  tocoYMeVoy billetera
+                          | otherwise = quedaIgual billetera
+
+transaccionCuatro :: Evento
+transaccionCuatro billetera | nombre billetera == "Luciano" =  ahorranteErrante billetera
+                            | otherwise = quedaIgual billetera
+
+transaccionCinco billetera | nombre billetera == "Jose"    = extraer (-7) billetera
+                           | nombre billetera == "Luciano" = depositar (7) billetera
+                           | otherwise                     = quedaIgual billetera
